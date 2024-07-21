5e80cac337cb7ae161004a7fe75e4a71f8ad08e9
Julimm1 <163172417+Julimm1@users.noreply.github.com>
2024-07-21 17:00:29 +0300

    add hw9

---
+++ src/js/2-form.js
@@ -0,0 +1,52 @@
+const form = document.querySelector('.feedback-form');
+
+const STORAGE_KEY = "feedback-form-state";
+
+let formData = {
+    email: "",
+    message: ""
+};
+
+populateForm();
+
+// Відстеження подій на формі
+form.addEventListener("submit", handleFormSubmit);
+form.addEventListener("input", handleFormInput);
+
+// Функція для обробки введення даних у форму
+function handleFormInput(event) {
+    const value = event.target.value.trim();
+    const key = event.target.name.trim();
+
+    formData[key] = value;
+    
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    
+    console.log(key, value);
+}
+
+function populateForm() {
+  let savedFeedbackData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  
+  if (!savedFeedbackData) {
+    return;
+  }
+
+  for (const key in savedFeedbackData) {
+      form.elements[key].value = savedFeedbackData[key];
+      formData[key] = savedFeedbackData[key];
+  }
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+  } else {
+    console.log(formData);
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+    formData = { email: "", message: "" };
+  }
+}
